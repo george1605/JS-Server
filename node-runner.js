64bef2df8906bd39b5b7f7fbbb57fb95e190c2a5
george1605 <32997165+george1605@users.noreply.github.com>
2022-08-12 21:55:52 +0300

    Create node-runner.js

---
+++ node-runner.js
@@ -0,0 +1,34 @@
+// something like: node-runner index.html
+const http = require('http');
+const fs = require('fs');
+const process = require('process');
+
+var server = {
+  port : 3000,
+  routes : {"/":(req) => {}},
+  create: function() {
+    http.createServer((req, res) => {
+      if(routes[res.url] == undefined)
+      {
+        res.write("Content-type: text/html");
+        res.write("<code>Cannot GET /" + req.url + "</code>");
+      }
+      res.write(this.routes[req.url](req));
+    }).listen(this.port);
+  }
+};
+
+var file = process.argv[1] || "index.html";
+
+server.routes["/"] = function(req)
+{
+  var x = "Content-type: text/html";
+  fs.readFile(file, (data) => {
+    data = data.replace(/<server-script>*</server-script>/g, function(s) {
+      s = s.replace("<server-script>","").replace("</server-script>","");
+      eval(s);
+    }
+  });
+}
+
+  
