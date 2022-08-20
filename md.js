1992dc76f63254ce196a639aa8cf57c4f74306d6
george1605 <32997165+george1605@users.noreply.github.com>
2022-08-20 23:15:06 +0300

    Update md.js

--- md.js
+++ md.js
@@ -4,3 +4,10 @@ function translateMD(x)
   n = n.replace("\n", "<br>");
   return n;
 }
+
+function translateLineMD(x)
+{
+  if(x.includes("# "))
+    x = "<h1>" + x.replace("# ","") + "</h1>";
+  return x;
+}
