2c8f5aa178bbbc0e8899207764b465a3f849a567
george1605 <32997165+george1605@users.noreply.github.com>
2022-08-22 16:20:48 +0300

    Create sql.php

---
+++ php/sql.php
@@ -0,0 +1,15 @@
+function sqlConnect($servername, $username, $password)
+{
+  $conn = new mysqli($servername, $username, $password);
+  if($conn->connect_error)
+    die("Cannot connect");
+  return $conn;
+}
+
+function sqlSelect($conn, $from = "table", $condition = NULL)
+{
+  $init = "SELECT * FROM " . $from;
+  if($condition != NULL)
+    $query = $init . "WHERE(" . $condition . ")";
+  return $conn->query($query);
+}
